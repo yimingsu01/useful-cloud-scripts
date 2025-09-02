a98b714033960038bfc921dd3561b5b715932743
Yiming Su <yiming34@illinois.edu>
2025-09-02 10:38:28 -0500

    added solarized light as default theme

--- nvim.bak/lua/plugins/minifiles.lua
+++ nvim.bak/lua/plugins/minifiles.lua
@@ -3,7 +3,7 @@ return {
     "folke/snacks.nvim",
     opts = {
       explorer = { enabled = false },
-    }
+    },
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -17,5 +17,18 @@ return {
         use_as_default_explorer = true,
       },
     },
+  },
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {},
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      vim.o.background = "light"
+      require("solarized").setup(opts)
+      vim.cmd.colorscheme("solarized")
+    end,
   },
 }
