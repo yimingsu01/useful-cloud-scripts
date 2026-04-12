afa56bdcc28850435444e2d31c2d8a33c72ecb89
Yiming Su <yiming34@illinois.edu>
2026-04-12 16:58:46 -0500

    update wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,15 +13,16 @@ config.initial_rows = 40
 -- or, changing the font size and color scheme.
 config.font_size = 14
 
-function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
-    return "Google Dark (base16)"
-  else
-    return "Google Light (base16)"
-  end
-end
+-- function scheme_for_appearance(appearance)
+--   if appearance:find "Dark" then
+--     return "Google Dark (base16)"
+--   else
+--     return "Google Light (base16)"
+--   end
+-- end
 
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.color_scheme = '3024 (base16)'
+-- config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.window_padding = {
   left = 2,
   right = 2,
