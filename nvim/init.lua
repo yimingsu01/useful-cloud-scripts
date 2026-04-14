086c3b553b2b8a4474815c7199dc45b014e320c4
Yiming Su <yiming34@illinois.edu>
2026-04-13 21:24:38 -0500

    track buffer change

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,10 +17,11 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "FocusLost", "BufLea
   command = "silent! update",
 })
 
--- ~/.config/nvim/lua/config/autocmds.lua
-
 -- Track the currently running `make` job, if any.
 local make_job = nil
+
+-- Track the last buffer change we already built for.
+local last_made_tick = {}
 
 -- Create an augroup so re-sourcing your config does not duplicate the autocmd.
 local group = vim.api.nvim_create_augroup("TexAutoMake", { clear = true })
@@ -28,9 +29,18 @@ local group = vim.api.nvim_create_augroup("TexAutoMake", { clear = true })
 vim.api.nvim_create_autocmd("InsertLeave", {
   group = group,
   pattern = "*.tex",
-  callback = function()
+  callback = function(args)
     -- Do nothing if `make` is not available on this system.
     if vim.fn.executable("make") ~= 1 then
+      return
+    end
+
+    local bufnr = args.buf
+    local tick = vim.b[bufnr].changedtick
+
+    -- If this buffer has not changed since the last time we ran `make`,
+    -- do nothing.
+    if last_made_tick[bufnr] == tick then
       return
     end
 
@@ -38,6 +48,9 @@ vim.api.nvim_create_autocmd("InsertLeave", {
     if make_job and vim.fn.jobwait({ make_job }, 0)[1] == -1 then
       return
     end
+
+    -- Remember that we already built this exact buffer state.
+    last_made_tick[bufnr] = tick
 
     -- Start `make` in Neovim's current working directory.
     -- Using a list avoids shell parsing.
