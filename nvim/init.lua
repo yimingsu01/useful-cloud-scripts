caf45c65611d65422846bd17db67b7ef52e2dd03
Yiming Su <yiming34@illinois.edu>
2026-04-13 20:12:53 -0500

    adds auto save and auto compile for latex

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,3 +11,36 @@ vim.lsp.config("ruff", {
 
 vim.lsp.enable("ruff")
 vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
+vim.opt.undofile = true
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "FocusLost", "BufLeave" }, {
+  pattern = "*",
+  command = "silent! update",
+})
+
+-- ~/.config/nvim/lua/config/autocmds.lua
+
+-- Track the currently running `make` job, if any.
+local make_job = nil
+
+-- Create an augroup so re-sourcing your config does not duplicate the autocmd.
+local group = vim.api.nvim_create_augroup("TexAutoMake", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = group,
+  pattern = "*.tex",
+  callback = function()
+    -- Do nothing if `make` is not available on this system.
+    if vim.fn.executable("make") ~= 1 then
+      return
+    end
+
+    -- If a previous `make` job is still running, do not start another one.
+    if make_job and vim.fn.jobwait({ make_job }, 0)[1] == -1 then
+      return
+    end
+
+    -- Start `make` in Neovim's current working directory.
+    -- Using a list avoids shell parsing.
+    make_job = vim.fn.jobstart({ "make" })
+  end,
+})
