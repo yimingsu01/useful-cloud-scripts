38c7774a9e5b59b55f3c919625506c6bf282e781
Yiming Su <yiming34@illinois.edu>
2026-04-20 14:48:57 -0500

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,21 +11,26 @@ config.initial_cols = 120
 config.initial_rows = 40
 
 -- or, changing the font size and color scheme.
-config.font_size = 14
+config.font_size = 13
 
--- function scheme_for_appearance(appearance)
---   if appearance:find "Dark" then
---     return "Google Dark (base16)"
---   else
---     return "Google Light (base16)"
---   end
--- end
+config.font = wezterm.font_with_fallback({
+  { family = 'JetBrains Mono', weight = 'DemiBold' },
+  'Symbols Nerd Font Mono',
+})
 
-config.color_scheme = '3024 (base16)'
--- config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return '3024 (base16)'
+  else
+    return "Solarized (light) (terminal.sexy)"
+  end
+end
+
+-- config.color_scheme = '3024 (base16)'
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.window_padding = {
-  left = 2,
-  right = 2,
+  left = 1,
+  right = 1,
   top = 0,
   bottom = 0,
 }
